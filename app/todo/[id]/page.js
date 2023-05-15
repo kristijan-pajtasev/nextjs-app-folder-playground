9ad1168cb1f7fc3bd8673ae4b99af8ed130cb753
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-05-16 00:36:35 +0100

    data

--- app/todo/[id]/page.js
+++ app/todo/[id]/page.js
@@ -1,10 +1,18 @@
 "use client"
-import {useEffect} from 'react'
+import {useEffect, use} from 'react'
+
+const getData = () => {
+  return new Promise((res, rej) => {
+    setTimeout(() => { res({}) }, 1500)
+  })
+}
+
 export default function ToDoDetail() {
   console.log("ToDo details page");
-
+  const data = use(getData());
+  console.log("data: ", data);
   useEffect(() => {
-    if(Math.random() < 0.30)
+    if(Math.random() < 0.01)
       throw new Error("ToDoDetails Error")
   }, [])
   return (
